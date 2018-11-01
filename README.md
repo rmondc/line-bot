9c4a097d1990042cdc09896206e1b918c761a4f1
Raymond Cheong <r_a_y_c@hotmail.com>
2018-11-01 20:09:07 +0900

    Commit

---
+++ README.md
@@ -0,0 +1,45 @@
+# LINE Bot 101
+
+## What we use
+- [LINE Messaging API](https://developers.line.me/en/docs/messaging-api/)
+- Heroku
+
+## NOTION!
+DO NOT INCLUDE 'line' in the name of provider and channel.
+If you do so, you cannnot create the provider nor the channel.
+
+## Installation
+```
+$ git clone git@github.com:hidehiro98/line-bot-101.git
+$ cd line-bot-101
+$ bundle install
+
+$ brew install heroku/brew/heroku
+$ heroku create $YOUR_APP_NAME
+
+$ heroku config:set LINE_CHANNEL_SECRET=$YOUR_CHANNEL_SECRET
+$ heroku config:set LINE_CHANNEL_TOKEN=$YOUR_CHANNEL_TOKEN
+Example
+$ heroku config:set LINE_CHANNEL_SECRET=f73d5df3fagu3g301856e1dc4cfcf3e1
+$ heroku config:set LINE_CHANNEL_TOKEN=FbKBF7cB1HReh9lIc6M3bDz8Rd6D+0f1kvBaJF93QadC7SsGpHP9K1EOOYkbwRThXHdVSSupJ4TgKMEtE/LbnE2heif2GZci+ntGdP89cGfrbLiofFFBlrFygi58f/B5UsvqkvlfNM7BHddRZhhV2RgdB04t89/1O/w1cDnyilFU=
+
+Only for image recognition
+$ heroku config:set IBM_IAM_API_KEY=$YOUR_IAM_API_KEY
+
+$ git push heroku master
+```
+
+## Slides
+https://www.slideshare.net/HidehiroNagaoka/le-wagon-tokyo-line-bot-101
+
+## Docs
+### Docs of LINE Messagin API
+- https://developers.line.me/en/docs/messaging-api/building-sample-bot-with-heroku/
+- https://github.com/line/line-bot-sdk-ruby
+
+### Docs of Sinatra
+- https://devcenter.heroku.com/articles/rack#sinatra
+
+### Docs of IBM Watson
+- https://console.bluemix.net/apidocs/visual-recognition?language=ruby
+- https://console.bluemix.net/dashboard/apps
---
+++ app.rb
@@ -0,0 +1,79 @@
+# app.rb
+require 'sinatra'
+require 'line/bot'
+require "json"
+require "ibm_watson/visual_recognition_v3"
+require 'tempfile'
+include IBMWatson
+
+def client
+  @client ||= Line::Bot::Client.new { |config|
+    config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+    config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+  }
+end
+
+post '/callback' do
+  body = request.body.read
+
+  signature = request.env['HTTP_X_LINE_SIGNATURE']
+  unless client.validate_signature(body, signature)
+    error 400 do 'Bad Request' end
+  end
+
+  events = client.parse_events_from(body)
+  events.each { |event|
+    case event
+    when Line::Bot::Event::Message
+      case event.type
+      when Line::Bot::Event::MessageType::Text
+        p event
+        user_id = event['source']['userId']
+        user_name = ''
+        response = client.get_profile(user_id)
+        case response
+        when Net::HTTPSuccess then
+          contact = JSON.parse(response.body)
+          p contact
+          user_name = contact['displayName']
+        else
+          p "#{response.code} #{response.body}"
+        end
+
+        message = {
+          type: 'text',
+          text: event.message['text'] + ', ' + user_name
+        }
+        client.reply_message(event['replyToken'], message)
+
+      # when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
+      when Line::Bot::Event::MessageType::Image
+        response = client.get_message_content(event.message['id'])
+        tf = Tempfile.open
+        tf.write(response.body)
+
+        visual_recognition = VisualRecognitionV3.new(
+          version: "2018-03-19",
+          iam_apikey: ENV["IBM_IAM_API_KEY"]
+        )
+
+        image_result = ''
+        File.open(tf.path) do |images_file|
+          classes = visual_recognition.classify(
+            images_file: images_file,
+            threshold: "0.6"
+          )
+          image_result = p classes.result['images'][0]['classifiers'][0]['classes'].to_s
+        end
+        message = {
+          type: 'text',
+          text: image_result
+        }
+        client.reply_message(event['replyToken'], message)
+        tf.unlink
+      end
+    end
+  }
+
+  "OK"
+end
